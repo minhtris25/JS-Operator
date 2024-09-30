d1568ccec6ca99747cedff9a27e4830c5cf1b7a0
minhtris25 <168254159+minhtris25@users.noreply.github.com>
2024-09-30 16:39:14 +0700

    Add files via upload

---
+++ JS-Operator/js/main.js
@@ -0,0 +1,19 @@
+var a= Number(prompt("nhập a"))
+var b= Number(prompt("nhập b"))
+var tong = a+b
+var hieu = a-b
+var tich = a*b
+var thuong = a/b
+document.write("tổng: "+tong+"<br>")
+document.write("hiệu: "+hieu+"<br>")
+document.write("tích: "+tich+"<br>")
+document.write("thương: "+thuong+"<br>")
+document.write("*************<br>")
+var c= a>b
+document.write(a+" > "+b+" = "+c+"<br>")
+var c= a<b
+document.write(a+" < "+b+" = "+c+"<br>")
+var c= a>=b
+document.write(a+" >= "+b+" = "+c+"<br>")
+var c= a<=b
+document.write(a+" <= "+b+" = "+c+"<br>")
